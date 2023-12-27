d622e4b024ba2fc377158e70253b3c8b34c3d07a
zhangyublue <912642673@qq.com>
2023-12-28 01:34:45 +0800

    feat: 添加reconciler

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,6 @@
+import { Props, Key } from 'shared/ReactTypes';
+import { WorkTag } from './workTags';
+
+export class Fiber {
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {}
+}
--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -55,4 +55,34 @@ export const jsx = (type: Element, config: any, ...maybeChildren: any) => {
 	return ReactElement(type, key, ref, props);
 };
 
-export const jsxDEV = jsx;
+export const jsxDEV = (type: Element, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+
+
